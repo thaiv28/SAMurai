ec677b409981b97880ca35c5013123f235b79c15
Thai Villaluna <thaivillaluna@gmail.com>
2024-09-09 16:50:25 -0700

    remove revoked secret

--- samurai/data/mining/pdf_scraper.py
+++ samurai/data/mining/pdf_scraper.py
@@ -12,4 +12,4 @@ def scrape_attachment(download_url: str):
     return text
     
 if __name__=="__main__":
-    print(scrape_attachment("https://sam.gov/api/prod/opps/v3/opportunities/resources/files/710cce66de59485396d1cc5e82dea2fc/download?&token=be8bdaa9-71a5-49f2-951f-a6cc749d3104"))
+    pass
